3b97b4b878a01a3e72704b5fa261013825a9d1b1
kerimcanbalkan <kerimcanbalkan@gmail.com>
2023-10-12 20:30:58 +0300

    configured mason and treesitter

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,15 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+         "lua_ls",
+         "cssls",
+         "emmet_language_server",
+         "eslint",
+         "html",
+         "omnisharp",
+         "svelte",
+         "tailwindcss",
+         "tsserver",
       })
     end,
   },
@@ -18,8 +26,8 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
+         "prettier",
+         "stylua",
       })
     end,
   },
